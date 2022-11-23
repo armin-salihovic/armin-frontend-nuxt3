81f705a809ab95d8f4e3ba1332a887395e4e0a90
Armin Salihovic <armin.salihovic@live.com>
2022-11-23 19:52:41 +0100

    Implement about page

--- pages/about.vue
+++ pages/about.vue
@@ -1,9 +1,33 @@
 <template>
-<h1>...about page</h1>
+  <div>
+    <article v-if="data">
+      <HeroImage :data="data" />
+      <div class="p-6 md:p-10 p-6 sm:px-12" v-html="data.data.content">
+      </div>
+    </article>
+    <Error404 v-else-if="error" />
+    <!--  <div class="pr-32 xl:pr-32 pr-0 gap-1 gap-2 gap-3 gap-4 gap-5 gap-6"></div>-->
+  </div>
 </template>
 
 <script setup>
 definePageMeta({
   layout: 'custom',
 })
+
+useHead({
+  title: `About | Armin Salihović`,
+});
+
+const route = useRoute();
+
+const projects = useState('about', () => []);
+
+const { data, pending, error, refresh } = await useFetch(`http://127.0.0.1:8000/api/about`, {
+  key: route.params.slug,
+})
 </script>
+
+<style scoped>
+
+</style>

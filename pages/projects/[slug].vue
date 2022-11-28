767d73c9985bc2a4631021c778a602cf455bbdda
Armin Salihovic <armin.salihovic@live.com>
2022-11-28 11:18:35 +0100

    Fix responsive hero image loading

--- pages/projects/[slug].vue
+++ pages/projects/[slug].vue
@@ -12,6 +12,7 @@ const { data, pending, error, refresh } = await useFetch(`http://127.0.0.1:8000/
 <template>
   <article v-if="data">
     <div class="article-content" v-html="data.data.content"></div>
+<!--    <div class="text-lg md:text-5xl text-3xl md:text-5xl text-2xl md:text-4xl text-gray-500"></div>-->
   </article>
   <Error404 v-else-if="error" />
 </template>

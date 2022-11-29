d07b185a7ef3c7653d76a84e726a98efcfc7ab8d
Armin Salihovic <armin.salihovic@live.com>
2022-11-29 15:48:27 +0100

    Move SEO settings from nuxt.config.ts to app.vue

--- app.vue
+++ app.vue
@@ -10,6 +10,17 @@ const settings = useState('settings', () => []);
 const { data , pending, error, refresh } = await useFetch(`http://127.0.0.1:8000/api/settings`);
 
 settings.value = data;
+
+useHead({
+  meta: [
+    { name: 'author', content: 'Armin Salihović' },
+    { property: 'og:type', content: 'website' },
+    { property: 'og:url', content: 'https://www.armin.ba' },
+  ],
+  link: [
+    { rel: 'icon', type: 'image/x-icon', href: settings.value['favicon'] }
+  ]
+})
 </script>
 
 <style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,19 +14,4 @@ export default defineNuxtConfig({
     css: [
         '@/assets/css/main.css',
     ],
-
-    app: {
-        head: {
-            meta: [
-                { name: 'author', content: 'Armin Salihović' },
-                { property: 'og:type', content: 'website' },
-                { property: 'og:url', content: 'https://www.armin.ba' },
-            ],
-            link: [
-                { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }
-            ]
-        }
-    },
-
-
 })
